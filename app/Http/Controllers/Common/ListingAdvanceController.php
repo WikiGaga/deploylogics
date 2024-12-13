5b7c021127501e4c07f364667516594788272165
asadilyas404 <asadilyas404@gmail.com>
2024-12-13 16:16:35 +0500

    asd

--- app/Http/Controllers/Common/ListingAdvanceController.php
+++ app/Http/Controllers/Common/ListingAdvanceController.php
@@ -34,6 +34,8 @@ class ListingAdvanceController extends Controller
 
     event(new PusherNotifyEvent('17580923022021','hello world','https://example.com/report'));
 
+        return response()->json('the end');
+
 
         $data = [];
         $case_name = (isset($subType) && !empty($subType)) ? $subType : $caseType;
