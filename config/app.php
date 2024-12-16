9d4bfe89a2b28a37651544179a0ef8b3d41f16d6
asadilyas404 <asadilyas404@gmail.com>
2024-12-16 11:42:16 +0500

    asd

--- config/app.php
+++ config/app.php
@@ -182,7 +182,6 @@ return [
         App\Providers\BladeServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
-        // Meneses\LaravelMpdf\LaravelMpdfServiceProvider::class,
     ],
 
     /*
@@ -236,7 +235,6 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Image' => Intervention\Image\Facades\Image::class,
         'Laratrust'   => Laratrust\LaratrustFacade::class,
-        // 'PDF' => Meneses\LaravelMpdf\Facades\LaravelMpdf::class,
         'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
         'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
