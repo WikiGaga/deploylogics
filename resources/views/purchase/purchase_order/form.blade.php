50aa00b0fdf796256a654c0152cbfe1f457ff193
asadilyas404 <asadilyas404@gmail.com>
2025-01-22 16:45:28 +0500

    asd

--- resources/views/purchase/purchase_order/form.blade.php
+++ resources/views/purchase/purchase_order/form.blade.php
@@ -109,14 +109,14 @@
                                         <input type="text" value="{{isset($supplier_code)?$supplier_code:''}}" data-url="{{action('Common\DataTableController@inlineHelpOpen','supplierHelp')}}" id="supplier_name" name="supplier_name" class="open_inline__help form-control erp-form-control-sm moveIndex" placeholder="Enter here">
                                         <input type="hidden" id="supplier_id" name="supplier_id" value="{{isset($supplier_id)?$supplier_id:''}}"/>
                                         <div class="input-group-append">
-                                                <button class="input-group-text btn-open-mob-help" id="mobOpenInlineSupplierHelp">
+                                                <span class="input-group-text btn-open-mob-help" id="mobOpenInlineSupplierHelp">
                                                 <i class="la la-search"></i>
-                                                </button>
+                                                </span>
                                         </div>
                                         <div class="input-group-append">
-                                                <button class="input-group-text" id="OpenInlineSupplierHelp">
+                                                <span class="input-group-text" id="OpenInlineSupplierHelp">
                                                 <i class="la la-search"></i>
-                                                </button>
+                                                </span>
                                         </div>
                                     </div>
                                     </div>
