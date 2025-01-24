ef562af718bb63d8f5fefd0ab66dc67912c15256
asadilyas404 <asadilyas404@gmail.com>
2025-01-24 17:28:00 +0500

    asd

--- resources/lang/ar/message.php
+++ resources/lang/ar/message.php
@@ -1,20 +1,19 @@
 <?php
 
-return [
-    'required_fields' => 'Fields are not validate.',
-    'not_barcode' => 'Product barcode is not exits',
-    'not_product' => 'Product is not exits',
-    'create' => 'Entry successfully created',
-    'update' => 'Entry successfully updated',
-    'delete' => 'Entry successfully deleted',
-    'not_delete' => 'Cannot delete the record',
-    'duplicate_record' => 'Duplicate record',
-    'report_ready' => 'Report ready',
-    'query_error' => 'Your Query Error',
-    'user_has_permission' => 'Any User has permission this record',
-    'error' => 'Something wrong..',
-    'already_login' => 'User Already login',
-    'fill_the_grid' => 'Fill The Grid',
-    'fill_the_grid' => 'Fill The Grid',
-    'smart_product' => 'المنتج الذكي',
-];
+return array (
+  'already_login' => 'User Already login',
+  'create' => 'Entry successfully created',
+  'delete' => 'Entry successfully deleted',
+  'duplicate_record' => 'Duplicate record',
+  'error' => 'Something wrong..',
+  'fill_the_grid' => 'Fill The Grid',
+  'not_barcode' => 'Product barcode does not exist',
+  'not_delete' => 'Cannot delete the record',
+  'not_product' => 'Product is not exits',
+  'query_error' => 'Your Query Error',
+  'report_ready' => 'Your Report is ready',
+  'required_fields' => 'Fields are not validate.',
+  'smart_product' => 'المنتج الذكي',
+  'update' => 'Entry successfully updated',
+  'user_has_permission' => 'Any User has permission this record',
+);
--- resources/lang/en/message.php
+++ resources/lang/en/message.php
@@ -1,20 +1,19 @@
 <?php
 
-return [
-    'required_fields' => 'Fields are not validate.',
-    'not_barcode' => 'Product barcode is not exits',
-    'not_product' => 'Product is not exits',
-    'create' => 'Entry successfully created',
-    'update' => 'Entry successfully updated',
-    'delete' => 'Entry successfully deleted',
-    'not_delete' => 'Cannot delete the record',
-    'duplicate_record' => 'Duplicate record',
-    'report_ready' => 'Report ready',
-    'query_error' => 'Your Query Error',
-    'user_has_permission' => 'Any User has permission this record',
-    'error' => 'Something wrong..',
-    'already_login' => 'User Already login',
-    'fill_the_grid' => 'Fill The Grid',
-    'smart_product' => 'Smart Product from translation',
-
-];
+return array (
+  'required_fields' => 'Fields are not validate.',
+  'not_barcode' => 'Product barcode is not exits',
+  'not_product' => 'Product is not exits',
+  'create' => 'Entry successfully created',
+  'update' => 'Entry successfully updated',
+  'delete' => 'Entry successfully deleted',
+  'not_delete' => 'Cannot delete the record',
+  'duplicate_record' => 'Duplicate record',
+  'report_ready' => 'Report ready',
+  'query_error' => 'Your Query Error',
+  'user_has_permission' => 'Any User has permission this record',
+  'error' => 'Something wrong..',
+  'already_login' => 'User Already login',
+  'fill_the_grid' => 'Fill The Grid',
+  'smart_product' => 'Smart Product',
+);
