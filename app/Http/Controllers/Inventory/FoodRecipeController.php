4afb7592b785043b38096f3bdfa3265cdd5cca9c
asadilyas404 <asadilyas404@gmail.com>
2025-08-05 11:32:52 +0500

    asd

--- app/Http/Controllers/Inventory/FoodRecipeController.php
+++ app/Http/Controllers/Inventory/FoodRecipeController.php
@@ -17,26 +17,30 @@ class FoodRecipeController extends Controller
         $data['page_data'] = [];
         $data['form_type'] = 'food-recipes';
         $data['page_data']['title'] = self::$page_title;
-        $data['page_data']['path_index'] = $this->prefixIndexPage.self::$redirect_url;;
-        $data['page_data']['create'] = '/'.self::$redirect_url.$this->prefixCreatePage;
-        if(isset($id)){
-            if(FoodRecipe::where('id','LIKE',$id)->exists()){
-                $data['permission'] = self::$menu_dtl_id.'-edit';
+        $data['page_data']['path_index'] = $this->prefixIndexPage . self::$redirect_url;
+        ;
+        $data['page_data']['create'] = '/' . self::$redirect_url . $this->prefixCreatePage;
+        if (isset($id)) {
+            if (FoodRecipe::where('id', 'LIKE', $id)->exists()) {
+                $data['permission'] = self::$menu_dtl_id . '-edit';
                 $data['page_data'] = array_merge($data['page_data'], Utilities::editForm());
                 $data['id'] = $id;
-                $data['current'] = FoodRecipe::with('dtls','food')->where('id',$id)->first();
+                $data['current'] = FoodRecipe::with('dtls', 'food')->where('id', $id)->first();
                 // dd($data['current']);
-                $data['page_data']['print'] = '/'.self::$redirect_url.'/print/'.$id;
-                // $data['document_code'] = $data['current']->id;
-            }else{
+                $data['page_data']['print'] = '/' . self::$redirect_url . '/print/' . $id;
+                $data['document_code'] = $data['current']->id;
+            } else {
                 abort('404');
             }
-        }else{
-            $data['permission'] = self::$menu_dtl_id.'-create';
+        } else {
+            $data['permission'] = self::$menu_dtl_id . '-create';
             $data['page_data'] = array_merge($data['page_data'], Utilities::newForm());
+
+            $maxId = FoodRecipe::max('id') ?? 0;
+            $data['document_code'] = $maxId + 1;
         }
 
-        return view('inventory.food-recipe.form',compact('data'));
+        return view('inventory.food-recipe.form', compact('data'));
     }
 
 }
