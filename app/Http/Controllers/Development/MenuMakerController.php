387d55075eb118f96afb1df37f4ee8acf9898b03
asadilyas404 <asadilyas404@gmail.com>
2025-08-07 13:33:21 +0500

    asd

--- app/Http/Controllers/Development/MenuMakerController.php
+++ app/Http/Controllers/Development/MenuMakerController.php
@@ -117,7 +117,7 @@ class MenuMakerController extends Controller
                     $has = in_array($permission->display_name, $request->views_action);
                     if($has == false){
                         $user_permission = PermissionUser::where('permission_id',$permission->id)->exists();
-                        dd($user_permission);
+                        dd(PermissionUser::where('permission_id',$permission->id)->get());
                         if($user_permission == true){
                             return $this->jsonErrorResponse($data, trans('message.user_has_permission'), 200);
                         }else{
