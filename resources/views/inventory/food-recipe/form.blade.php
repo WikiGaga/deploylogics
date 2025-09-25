4fb3a2fa7724766d9dd84737fcb42dfad7e08ef7
asadilyas404 <asadilyas404@gmail.com>
2025-09-25 13:00:13 +0500

    asd

--- resources/views/inventory/food-recipe/form.blade.php
+++ resources/views/inventory/food-recipe/form.blade.php
@@ -97,13 +97,13 @@
                                     <div class="input-group open-modal-group">
                                         <input id="food_name" name="food_name" value="{{ isset($option_name) ? $option_name : '' }}"
                                             type="text" class="form-control erp-form-control-sm" readonly>
-                                        {{-- @if ($case == 'new')
+                                        @if ($case == 'new')
                                             <div class="input-group-append">
                                                 <span class="input-group-text group-input-btn" id="getFoodDetailData">
                                                     GO
                                                 </span>
                                             </div>
-                                        @endif --}}
+                                        @endif
                                     </div>
                                 </div>
                             </div>
