0653a12d26287161f0ff2022bd55286bd9b2ad56
asadilyas404 <asadilyas404@gmail.com>
2025-10-12 00:13:22 +0500

    asd

--- app/Http/Controllers/Accounts/CoaController.php
+++ app/Http/Controllers/Accounts/CoaController.php
@@ -113,9 +113,9 @@ class CoaController extends Controller
 
         //supplier and customer not open in chart of accouunt
         $supplier_group = "";
-dd($request->chart_code);
 
         if(isset(Session::get('dataSession')->supplier_group) && !empty(Session::get('dataSession')->supplier_group)){
+            dd(Session::get('dataSession')->supplier_group);
             $chart_supplier_group = TblAccCoa::where('chart_Account_id',Session::get('dataSession')->supplier_group)->where(Utilities::currentBC())->first('chart_code');
             $supplier_group = substr($chart_supplier_group->chart_code,0,4);
         }
