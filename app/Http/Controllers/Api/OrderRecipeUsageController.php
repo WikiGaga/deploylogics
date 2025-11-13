123af2b5277e0ea7d8f7f4e26f4d3c3ed0f12d15
asadilyas404 <asadilyas404@gmail.com>
2025-11-13 15:46:02 +0500

    asd

--- app/Http/Controllers/Api/OrderRecipeUsageController.php
+++ app/Http/Controllers/Api/OrderRecipeUsageController.php
@@ -159,7 +159,6 @@ class OrderRecipeUsageController extends Controller
 
         try {
             foreach ($rowsToPersist as $row) {
-                dd($row['food_id']);
                 OrderRecipeUsage::updateOrCreate(
                     [
                         'order_detail_id' => $row['order_detail_id'],
