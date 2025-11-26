adf4f0bc511daa624e031581eed88aa15b25f2dd
asadilyas404 <asadilyas404@gmail.com>
2025-11-26 13:00:11 +0500

    asd

--- resources/views/prints/partial/footer.blade.php
+++ resources/views/prints/partial/footer.blade.php
@@ -1,7 +1,7 @@
 <table width="100%" style="border-top: 1px solid #000;">
     <tbody>
     <tr>
-        <td width="50%">Powered by <span>Royalsoft</span></td>
+        <td width="50%">Powered by <span>Deploylogics</span></td>
         <td width="50%" style="text-align:right;">Print Date & Time: {{date("d-m-Y h:i:s")}}</td>
     </tr>
     </tbody>
