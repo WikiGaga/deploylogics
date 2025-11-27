002815606f9a233fb7e0db9e29e26863d2133faa
asadilyas404 <asadilyas404@gmail.com>
2025-11-27 13:44:23 +0500

    asd

--- resources/views/elements/page_header.blade.php
+++ resources/views/elements/page_header.blade.php
@@ -3,6 +3,20 @@
     <span class="kt-portlet__head-icon">
         <i class="kt-font-brand flaticon2-file"></i>
     </span>
+    <h3 class="kt-portlet__head-title">
+        @php
+            $title = isset($page_data['title']) ? $page_data['title'] : '';
+            $title_key = 'message.' . strtolower(str_replace([' ', '-'], '_', $title));
+            $translated_title = __($title_key);
+            $display_title = ($translated_title !== $title_key) ? $translated_title : $title;
+
+            $type = isset($page_data['type']) ? $page_data['type'] : '';
+            $type_key = 'message.' . strtolower($type);
+            $translated_type = __($type_key);
+            $display_type = ($translated_type !== $type_key) ? $translated_type : ucwords($type);
+        @endphp
+        {{ $display_title }}<small class="text-capitalize">{{ $display_type }}</small>
+    </h3>
     <div class="erp-page--actions">
         @if(isset($page_data['action']) && $page_data['action'] != '')
             <button type="submit" id="btn-update-entry" class="btn btn-sm btn-success">{{$page_data['action']}}</button>
