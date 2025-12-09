eb8581721df41b2724e68f0bbb32304900f5e82c
asadilyas404 <asadilyas404@gmail.com>
2025-12-09 14:55:59 +0500

    asd

--- public/js/pages/js/common/multi-products-ajax.js
+++ public/js/pages/js/common/multi-products-ajax.js
@@ -103,7 +103,9 @@ var KTDatatableRemoteAjaxDemo = function() {
                         var product_barcode_packing = row.product_barcode_packing;
                         var col = "<div>";
                         col += "<div><span style=' width: 40px; display: inline-block; font-weight: 500; '>Name:</span> "+product_name+"</div>";
+                        if(product_arabic_name) {
                             col += "<div><span style=' width: 40px; display: inline-block; font-weight: 500; '>Arabic:</span> "+product_arabic_name+"</div>";
+                        }
                         col += "<div><span style=' width: 40px; display: inline-block; font-weight: 500; '>UOM:</span> "+uom_name +" <span style=' width: 40px; display: inline-block; font-weight: 500; '> PACK:</span> "+product_barcode_packing+"</div>";
                        // col += "<div><span style=' width: 60px; display: inline-block; font-weight: 500; '>Category:</span> "+product_type_name+"</div>";
                         col += "</div>";
@@ -307,7 +309,7 @@ var KTDatatableRemoteAjaxDemo = function() {
                                     '      <span style="width: 200px;">\n' +
                                     '         <div>\n' +
                                     '            <div><span style=" width: 40px; display: inline-block; font-weight: 500; ">Name:</span> '+notNull(item['product_name'])+'</div>\n' +
-                                    '<div><span style=" width: 40px; display: inline-block; font-weight: 500; ">Arabic:</span> '+notNull(item['product_arabic_name'])+'</div>\n' +
+                                    (item['product_arabic_name'] ? '            <div><span style=" width: 40px; display: inline-block; font-weight: 500; ">Arabic:</span> '+notNull(item['product_arabic_name'])+'</div>\n' : '') +
                                     '            <div><span style=" width: 40px; display: inline-block; font-weight: 500; ">UOM:</span> '+notNull(item['uom_name'])+' <span style=" width: 40px; display: inline-block; font-weight: 500; "> PACK:</span> '+notNull(item['product_barcode_packing'])+'</div>\n' +
                                     '         </div>\n' +
                                     '      </span>\n' +
