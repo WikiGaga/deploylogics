e75ae3aee94a6a291ed475eacc36797ed14ff0a8
asadilyas404 <asadilyas404@gmail.com>
2025-12-09 13:58:08 +0500

    asd

--- app/Http/Controllers/Common/DataTableController.php
+++ app/Http/Controllers/Common/DataTableController.php
@@ -1093,7 +1093,7 @@ class DataTableController extends Controller
         if (in_array($helpType, $listHelpProducts)) {
             $data['show_name'] = 'product_barcode_barcode';
             $data['hideKeys'] = ['product_id', 'product_barcode_id', 'uom_id'];
-            $data['keys'] = ['product_barcode_barcode', 'product_name', 'uom_name', 'product_barcode_packing'];
+            $data['keys'] = ['product_barcode_barcode', 'product_name', 'product_arabic_name', 'uom_name', 'product_barcode_packing'];
             $merge = array_merge($data['keys'], $data['hideKeys']);
             $selectColumns = implode(', ', $merge);
 
@@ -1164,7 +1164,7 @@ FETCH FIRST 50 ROWS ONLY";
 
                 $data['list'] = DB::select($qry);
             }
-            $data['head'] = ['Barcode', 'Name', 'UOM', 'Packing'];
+            $data['head'] = ['Barcode', 'Name', 'Arabic Name', 'UOM', 'Packing'];
         }
         if ($helpType == 'productHelpSI') {
             $data['show_name'] = 'product_barcode_barcode';
