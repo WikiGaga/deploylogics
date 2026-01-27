d98f22345f78b2d3593d729469f0c8b29af7b89b
Zaryab Akhtar <zaryabakhtar9@gmail.com>
2026-01-27 18:13:07 +0400

    Update report.blade.php

--- resources/views/layouts/report.blade.php
+++ resources/views/layouts/report.blade.php
@@ -421,15 +421,15 @@ $(document).ready(function() {
                                     variationsHtml += '<div class="variation-value ml-1">';
                                     if(value.is_deleted == 'Y'){
                                         if(variation.printing_option == 'option_list_name'){
-                                            variationsHtml += '- <del>' + variation.name + '</del>';
-                                        }else{
                                             variationsHtml += '- <del>' + value.label + '</del>';
+                                        }else{
+                                            variationsHtml += '- <del>' + variation.name + '</del>';
                                         }
                                     }else{
                                         if(variation.printing_option == 'option_list_name'){
-                                            variationsHtml += '- ' + variation.name + '</strong>';
-                                        }else{
                                             variationsHtml += '- ' + value.label + '</strong>';
+                                        }else{
+                                            variationsHtml += '- ' + variation.name + '</strong>';
                                         }
                                     }
                                     variationsHtml += '</div>';
