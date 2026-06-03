bbb9849a19a0387c9ed2c9ec963eb013d57c2ad9
Musama1221 <o26s26m@gmail.com>
2026-06-03 17:49:35 +0500

    comit

--- resources/views/accounts/cash_voucher/form.blade.php
+++ resources/views/accounts/cash_voucher/form.blade.php
@@ -213,6 +213,21 @@
                             </div>
                         </div>
                         @else
+                         <div class="col-lg-4">
+                            <div class="row">
+                                <label class="col-lg-6 erp-col-form-label">Select Branch:</label>
+                                <div class="col-lg-6">
+                                    <select class="form-control erp-form-control-sm moveIndex kt-select2" id="new_branch_id" name="" disabled>
+                                        
+                                        @foreach($user_branches as $branch)
+                                            <option value="{{$branch->branch_id}}" {{$data['current']->branch_id == $branch->branch_id ? 'selected' : ''}}>
+                                                {{$branch->branch_name}}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
                             <input type="hidden" id="new_branch_id" name="new_branch_id" value="{{ $data['current']->branch_id }}">
                         @endif
 
