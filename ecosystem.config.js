608de9b9a2fa494731e379159580dae6988307f9
Asad Ilyas <asadilyas404@gmail.com>
2026-08-31 14:26:45 +0500

    ecosystem file add for queue

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [{
+      name: 'deploylogics-whatsapp-queue',
+      cwd: '/var/www/royalerp.net/deploylogics',
+      script: 'artisan',
+      interpreter: 'php',
+      args: 'queue:work database --queue=whatsapp --sleep=3 --tries=3 --timeout=60 --max-time=3600',
+      autorestart: true,
+      max_restarts: 20,
+      restart_delay: 5000,
+      out_file: './storage/logs/pm2-queue-out.log',
+      error_file: './storage/logs/pm2-queue-error.log',
+      merge_logs: true,
+      time: true,
+    }]
+  };
